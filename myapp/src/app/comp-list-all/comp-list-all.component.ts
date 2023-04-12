d3d54f20ec6b039d862311133b8ba3f80bb0d8c0
Spoods <75pulsar75@gmail.com>
2023-04-12 11:55:48 -0400

    naaded lists, will dynamically add items

--- myapp/src/app/comp-list-all/comp-list-all.component.ts
+++ myapp/src/app/comp-list-all/comp-list-all.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class CompListAllComponent {
 
+newDue = -1
+newName = "Default"
+newPrice = -1.11
+newMonthly = false
+newPayDay = -1
+
+currNames= ["Netflix - Premium", "Disney+ - Ad Free"]
+currDaysTillDue = [31, 25]
+currPayDays = [31, 31]
+currPrice = [19.99, 10.99]
+
+
+presNames =  ["Netflix - Basic Ad Free", "Netflix - Basic w/ Ads", "Netflix - Standard",
+              "Netflix - Premium", "Disney+ w/ Ads", "Disney+ - Ad Free"]
+presDaysTillDue = ["MON", "MON", "MON", "MON", "MON", "MON"]
+presPrice = [9.99, 6.99, 15.49,
+              19.99, 7.99, 10.99] 
+
+              addItem(){
+
+              }
+
+              removeItem(){
+                
+              }
+
 }
