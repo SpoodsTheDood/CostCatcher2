3915ccf463551254f067160275b624441268d556
Spoods <75pulsar75@gmail.com>
2023-05-17 10:18:33 -0400

    variable name change

--- myapp/src/app/comp-list-all/comp-list-all.component.ts
+++ myapp/src/app/comp-list-all/comp-list-all.component.ts
@@ -11,7 +11,6 @@ import { ServTestService } from '../servtest.service';
 })
 export class CompListAllComponent {
 
-
   url = 'http://localhost:3000/services/64494896c7234c797a7e1fa3'
   constructor(private httpClient:HttpClient, servTestService:ServTestService){}
   
@@ -38,18 +37,31 @@ newMonthly = false
 newPayDay = -1
 
 //These are the ones that will show up inside the lists
-currNames:JSON[] = []
-currDaysTillDue:JSON[] = []
-currPayDays:JSON[] = []
-currPrice:JSON[] = []
+jsNames:JSON[] = []
+jsDaysTillDue:JSON[] = []
+jsPayDays:JSON[] = []
+jsPrice:JSON[] = []
+currNames:String[] = ["One", "Two", "Three", "Four"]
+currDaysTillDue:Number[] = [1, 2, 3, 1]
+currPayDays:String[] = ["MON", "BIWK", "MON", "WKLY"]
+currPrice:Number[] = [1.11, 2.22, 3.33, 4.44]
 
 
 loadServices(){
-  console.log('Beginning load')
-  this.httpClient.get(this.url)
+  var mashup = []
+  for (var j = 0; j < this.currNames.length; j++){
+    mashup.push({'name': this.currNames[j], 'dueIn': this.currDaysTillDue[j], 'price':this.currPrice[j]})
+  }
+  mashup.sort(function(a, b){
+    return ((a.name < b.name) ? -1 : ((a.name == b.name) ? 0 : 1))
+  })
+  for (var k = 0; k < mashup.length; k++) {
+    this.currNames[k] = mashup[k].name;
+    this.currDaysTillDue[k] = mashup[k].dueIn;
+    this.currPrice[k] = mashup[k].price
+}
   
 }
-
 
 
 /*async fetchData() {
