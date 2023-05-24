5403be16676dbe38ca1ba6a11585f7f76fc16a6d
Spoods <75pulsar75@gmail.com>
2023-05-24 11:55:21 -0400

    added dropdown to change routes (oh also routes)

--- myapp/src/app/app-routing.module.ts
+++ myapp/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CompMakeServiceComponent } from './comp-make-service/comp-make-service.component';
 
 const routes: Routes = [
-  { path: '', component:AppComponent}
+  { path: '', component:AppComponent },
+  { path: 'add', component:CompMakeServiceComponent}
 ];
 
 @NgModule({
