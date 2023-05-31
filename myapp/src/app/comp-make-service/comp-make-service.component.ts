f26a225de7f040a94d5e5aeb69b9cd7078b237b6
SCBattisti <104858971+SCBattisti@users.noreply.github.com>
2023-05-31 11:18:02 -0400

    post functionality

--- myapp/src/app/comp-make-service/comp-make-service.component.ts
+++ myapp/src/app/comp-make-service/comp-make-service.component.ts
@@ -1,9 +1,10 @@
 //alright im overwhelmed, i'm making comments
 import { Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ServTestService } from '../servtest.service';
+
 import { CompListAllComponent } from '../comp-list-all/comp-list-all.component';
 // ^ most variables are saved in list-all for the sole reason of myconvenience
-import { ServTestService } from '../servtest.service';
 import {MatNativeDateModule} from '@angular/material/core'
 import {MatDatepickerModule} from '@angular/material/datepicker'
 import {MatInputModule} from '@angular/material/input';
@@ -19,13 +20,26 @@ export class CompMakeServiceComponent {
 //these bad boys are for when you make a new service
 //will not be used outside of this component
 
-url = 'http://localhost:3000/services/64494896c7234c797a7e1fa3'
-    constructor(private httpClient:HttpClient,
+// the specific url for the user we are adding a service to... id is '646638e8c7b9a011beca82c1'
+url = 'http://localhost:3000/services/646638e8c7b9a011beca82c1'
+    /*constructor(private httpClient:HttpClient,
       serviceTest: ServTestService){}
 
       getHeapCodeStatistics(){
         return this.httpClient.get(this.url)
-      }
+      }*/
+constructor(private servTestService: ServTestService) {}
+
+createService(serviceInfo: Object) {
+  this.servTestService.post(this.url, serviceInfo).subscribe((response:any) => {
+    console.log(response)
+
+  })
+
+}
+
+
+
 newDue = "-1"
 newName = "Default"
 newPrice = "-1.11"
--- myapp/src/app/servtest.service.ts
+++ myapp/src/app/servtest.service.ts
@@ -6,11 +6,20 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ServTestService {
   private url = 'http://localhost:3000/services/user/646638e8c7b9a011beca82c1'
+  private deleteUrl = ""
 
   constructor(private httpClient: HttpClient) { }
 
   getPosts() {
     return this.httpClient.get(this.url)
   }
+
+  post(url: String, payload: Object) {
+    return this.httpClient.post(this.url, payload)
+  }
+  delete() {
+    return this.httpClient.delete(this.deleteUrl)
+  }
+  
 
 }
